4bc76461d6984a686b5093fd9566c8b3c643e6d9
justinprabhakaran <c.m.justinprabhakaran@gmail.com>
2024-12-01 09:49:44 +0530

    added projectmanagement page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 
 import UserManagementPage from "@/pages/UserManagementPage.tsx";
+import {ProjectManagement} from "@/pages/ProjectManagement.tsx";
 
 export default function App() {
     return (
         <>
-            <UserManagementPage/>
+            <ProjectManagement />
         </>
     );
 }
